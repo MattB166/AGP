069f5201cbff0c584f9f4192b75b658869316e5d
MattB166 <matthewbrake2001@hotmail.co.uk>
2024-09-25 20:04:24 +0100

    week 1 setup

---
+++ AGP Week 1/main.cpp
@@ -0,0 +1,10 @@
+#include <Windows.h>
+
+int main()
+{
+	////global variables
+
+	HINSTANCE g_hInst = NULL;   // Handle to this instance
+	HWND g_hWnd = NULL;         // Handle to our created window
+	const wchar_t* windowName = L"DirectX 11 Tutorial"; // Name of our window
+}
