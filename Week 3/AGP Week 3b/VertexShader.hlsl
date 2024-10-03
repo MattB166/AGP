f1554489603df4712c26ec1b25c843a251be4489
MattB166 <matthewbrake2001@hotmail.co.uk>
2024-10-03 17:00:23 +0100

    shader setup to return colour

--- Week 3/AGP Week 3b/VertexShader.hlsl
+++ Week 3/AGP Week 3b/VertexShader.hlsl
@@ -1,4 +1,19 @@
-float4 main( float4 pos : POSITION ) : SV_POSITION
+struct Vin
 {
-	return pos;
+    float4 pos : POSITION;
+    float4 colour : COLOR;
+};
+struct Vout
+{
+    float4 pos : SV_POSITION;
+    float4 colour : COLOR;
+
+};
+
+Vout main(Vin Input)
+{
+    Vout output;
+    output.pos = Input.pos;
+    output.colour = Input.colour;
+    return output;
 }
