1ddb716e71739838aee722b2448340db164fa475
DanSheshtanov <d.sheshtanov@gmail.com>
2024-11-24 01:56:17 +0000

    your problemo

--- Code Cleanup And Class Creation/AGP Cleanup/Source/GameObject/GameObject.cpp
+++ Code Cleanup And Class Creation/AGP Cleanup/Source/GameObject/GameObject.cpp
@@ -84,15 +84,32 @@ void GameObject::UpdateConstantBuffer(ID3D11DeviceContext* g_devcon, const XMMAT
 	m_cBuffer.world = XMMatrixTranspose(m_transform.GetWorldMatrix());
 	m_cBuffer.view = XMMatrixTranspose(view);
 	m_cBuffer.projection = XMMatrixTranspose(projection);
-	g_devcon->UpdateSubresource(m_CBuffer, 0, 0, &m_CBuffer, 0, 0);
-	g_devcon->VSSetConstantBuffers(0, 1, &m_CBuffer);
-}
-
-void GameObject::CreateConstantBuffer(ID3D11Device* g_dev)
-{
-	D3D11_BUFFER_DESC cBufferDesc = {};
-	cBufferDesc.Usage = D3D11_USAGE_DEFAULT;
-	cBufferDesc.ByteWidth = sizeof(CBuffer);
+	g_devcon->UpdateSubresource(m_CBuffer, 0, 0, &m_CBuffer, 0, 0); // This is doo doo.
+	// UpdateSubresource(destination, 0, 0, source, 0, 0)
+	// Now your call here doesnt make sense as your dst and src are both the same.
+	// You're effectively copying data A into data A...
+	// If you look in Renderer::RenderFrame, you will see that you are already calling UpdateSubresource(..)
+	// but the difference is you're creating a local cbuffer variable, setting data inside it, THEN
+	// copying it into the ID3D11Buffer. So first order of business, is decide if you wanna update and set
+	// the cbuffer here in the GameObject, or RenderFrame. I'm kinda leaning to here, but you know your code base better.
+	// 
+	// Now the other issue, dx error:
+	// "The size of the Constant Buffer at slot 0 of the Vertex Shader unit is too small (192 bytes provided, 496 bytes, at least, expected)."
+	// This suggests that you are setting the wrong cbuffer in VSSetConstantBuffers(..).
+	// Your shader is expecting the cbuffer containing lighting info, very same one you are also using in RenderFrame, so that's good.
+	// However, you are changing it to the wrong cbuffer on the line below as m_CBuffer is created here ---------
+	// This CBuffer type below is a struct inside of GameObject.h, and does not									|
+	// match the one your shader expects (expected 496 bytes but given 192).									|
+	// So the other thing is to make sure you are using the same cbuffer in hlsl and cpp						|
+	//																											|
+	g_devcon->VSSetConstantBuffers(0, 1, &m_CBuffer);							//								|
+}																				//								|
+																				//								|
+void GameObject::CreateConstantBuffer(ID3D11Device* g_dev)						//								|
+{																				//								|
+	D3D11_BUFFER_DESC cBufferDesc = {};											//								|
+	cBufferDesc.Usage = D3D11_USAGE_DEFAULT;									//								|
+	cBufferDesc.ByteWidth = sizeof(CBuffer); // <----- 192 bytes ------------------------------------------------
 	cBufferDesc.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
 	cBufferDesc.CPUAccessFlags = 0;
 	HRESULT hr = g_dev->CreateBuffer(&cBufferDesc, NULL, &m_CBuffer);
