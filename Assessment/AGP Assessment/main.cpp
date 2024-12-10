59cd774541e03cabe429a04b1d0c715ac5773b21
MattB166 <matthewbrake2001@hotmail.co.uk>
2024-12-10 23:42:45 +0000

    directxtk installed and linker dependencies added

--- Assessment/AGP Assessment/main.cpp
+++ Assessment/AGP Assessment/main.cpp
@@ -1,13 +1,18 @@
 #include <Windows.h>
 #include <d3d11.h>
 #include <iostream>
-//#include "imgui.h" TBD 
+
 HINSTANCE hInstance = NULL;
 HWND hwnd = NULL;
 
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow) 
 {
 	//left off here, need to give program an "application" instance to run. 
+
+	//so "Application" instance - > run. 
+	//application holds window, renderer and imgui. 
+
+	return 0; 
 }
 
 	
