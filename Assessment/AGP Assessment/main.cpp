a62eb46e8c7c35bd849b993c97c6f7fcc279d608
MattB166 <matthewbrake2001@hotmail.co.uk>
2024-12-10 21:40:00 +0000

    set up assessment proj

---
+++ Assessment/AGP Assessment/main.cpp
@@ -0,0 +1,13 @@
+#include <Windows.h>
+#include <d3d11.h>
+#include <iostream>
+//#include "imgui.h" TBD 
+HINSTANCE hInstance = NULL;
+HWND hwnd = NULL;
+
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow) 
+{
+	//left off here, need to give program an "application" instance to run. 
+}
+
+	
