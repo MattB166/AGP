8f7bfbb3eb5ba31dcdb0135a04f514802b2a5ddf
MattB166 <matthewbrake2001@hotmail.co.uk>
2024-12-16 14:58:47 +0000

    model font and textures sorted in asset manager, just need to do shaders

--- Assessment/AGP Assessment/Source/AssetManager/AssetManager.cpp
+++ Assessment/AGP Assessment/Source/AssetManager/AssetManager.cpp
@@ -3,13 +3,35 @@
 ID3D11Device* AssetManager::m_dev = nullptr;
 ID3D11DeviceContext* AssetManager::m_devcon = nullptr;
 
+std::unordered_map<const wchar_t*, std::shared_ptr<Material>> AssetManager::m_materials;
+std::unordered_map<const wchar_t*, std::shared_ptr<ObjFileModel>> AssetManager::m_models;
+std::unordered_map<const wchar_t*, std::shared_ptr<SpriteFont>> AssetManager::m_fonts;
+
+
 void AssetManager::Initialize(ID3D11Device* dev, ID3D11DeviceContext* devcon)
 {
 	m_dev = dev;
 	m_devcon = devcon;
 }
 
-std::shared_ptr<Material> AssetManager::CreateMaterial(ID3D11Device* dev, ID3D11DeviceContext* devcon, const wchar_t* texturePath)
+void AssetManager::CleanUp()
+{
+	//clear all maps 
+	GetMaterials().clear();
+
+	if (m_dev)
+	{
+		delete m_dev;
+		m_dev = nullptr;
+	}
+	if (m_devcon)
+	{
+		delete m_devcon;
+		m_devcon = nullptr;
+	}
+}
+
+std::shared_ptr<Material> AssetManager::CreateMaterial(const wchar_t* texturePath)
 {
 	if (IsMaterialLoaded(*texturePath))
 	{
@@ -18,12 +40,33 @@ std::shared_ptr<Material> AssetManager::CreateMaterial(ID3D11Device* dev, ID3D11
 	else
 	{
 		ID3D11ShaderResourceView* texture = nullptr;
-		DirectX::CreateWICTextureFromFile(dev, devcon, texturePath, nullptr, &texture);
-		std::shared_ptr<Material> material = std::make_shared<Material>(dev, devcon, texture);
+		DirectX::CreateWICTextureFromFile(m_dev, m_devcon, texturePath, nullptr, &texture);
+		std::shared_ptr<Material> material = std::make_shared<Material>(m_dev, m_devcon, texture);
 		GetMaterials().insert(std::make_pair(texturePath, material));
 		return material;
 	}
 
+}
+
+std::shared_ptr<ObjFileModel> AssetManager::CreateModel(const wchar_t* modelPath)
+{
+	if (IsModelLoaded(*modelPath))
+	{
+		return RetrieveModel(*modelPath);
+	}
+	else
+	{
+		std::shared_ptr<ObjFileModel> model = std::make_shared<ObjFileModel>((char*)modelPath, m_dev, m_devcon);
+		GetModels().insert(std::make_pair(modelPath, model));
+		return model;
+
+	}
+	
+}
+
+std::shared_ptr<SpriteFont> AssetManager::MakeFont(const wchar_t* fontPath)
+{
+	return std::shared_ptr<SpriteFont>();
 }
 
 bool AssetManager::IsMaterialLoaded(const wchar_t& texturePath)
@@ -38,6 +81,28 @@ bool AssetManager::IsMaterialLoaded(const wchar_t& texturePath)
 	return false;
 }
 
+bool AssetManager::IsModelLoaded(const wchar_t& modelPath)
+{
+	for (const auto& model : GetModels())
+	{
+		if (wcscmp(model.first, &modelPath) == 0)
+		{
+			return true;
+		}
+	}
+}
+
+bool AssetManager::IsFontLoaded(const wchar_t& fontPath)
+{
+	for (const auto& font : GetFonts())
+	{
+		if (wcscmp(font.first, &fontPath) == 0)
+		{
+			return true;
+		}
+	}
+}
+
 std::shared_ptr<Material> AssetManager::RetrieveMaterial(const wchar_t& texturePath)
 {
 	auto it = GetMaterials().find(&texturePath);
@@ -46,5 +111,24 @@ std::shared_ptr<Material> AssetManager::RetrieveMaterial(const wchar_t& textureP
 		return it->second;
 	}
 	return nullptr; 
+}
+
+std::shared_ptr<ObjFileModel> AssetManager::RetrieveModel(const wchar_t& modelPath)
+{
+	auto it = GetModels().find(&modelPath);
+	if (it != GetModels().end())
+	{
+		return it->second;
+	}
+	return nullptr; 
+}
+
+std::shared_ptr<SpriteFont> AssetManager::RetrieveFont(const wchar_t& fontPath)
+{
+	auto it = GetFonts().find(&fontPath);
+	if (it != GetFonts().end())
+	{
+		return it->second;
+	}
 }
 
--- Assessment/AGP Assessment/Source/AssetManager/AssetManager.h
+++ Assessment/AGP Assessment/Source/AssetManager/AssetManager.h
@@ -2,40 +2,68 @@
 #include <d3d11.h>
 #include <memory>
 #include <unordered_map>
+#include "SpriteFont.h"
 #include "../Component/Material.h"
+#include "../ObjectFileModel/objfilemodel.h"
 class AssetManager
 {
 public:
 	static void Initialize(ID3D11Device* dev, ID3D11DeviceContext* devcon);
+	static void CleanUp();
 
 #pragma region Externals
-	static std::shared_ptr<Material> CreateMaterial(ID3D11Device* dev, ID3D11DeviceContext* devcon, const wchar_t* texturePath);
+	static std::shared_ptr<Material> CreateMaterial(const wchar_t* texturePath);
+
+	static std::shared_ptr<ObjFileModel> CreateModel(const wchar_t* modelPath);
+
+	static std::shared_ptr<SpriteFont> MakeFont(const wchar_t* fontPath);
 
 
 #pragma endregion
+
 private:
+#pragma region Members
 	static ID3D11Device* m_dev;
 	static ID3D11DeviceContext* m_devcon;
+#pragma endregion
 
+#pragma region ConstructorsAndOperators
 	AssetManager() = delete;
 	~AssetManager() = delete;
 	AssetManager(const AssetManager&) = delete;
 	AssetManager& operator=(const AssetManager&) = delete;
+#pragma endregion	
 
 #pragma region InternalChecks
 	static bool IsMaterialLoaded(const wchar_t& texturePath);
+
+	static bool IsModelLoaded(const wchar_t& modelPath); 
+
+	static bool IsFontLoaded(const wchar_t& fontPath);
 #pragma endregion
 
 #pragma region InternalRetrievals
 	static std::shared_ptr<Material> RetrieveMaterial(const wchar_t& texturePath);
+
+	static std::shared_ptr<ObjFileModel> RetrieveModel(const wchar_t& modelPath);
+
+	static std::shared_ptr<SpriteFont> RetrieveFont(const wchar_t& fontPath);
 #pragma endregion
 
 #pragma region Maps
 	static std::unordered_map<const wchar_t*, std::shared_ptr<Material>> m_materials;
+
+	static std::unordered_map<const wchar_t*, std::shared_ptr<ObjFileModel>> m_models;
+
+	static std::unordered_map<const wchar_t*, std::shared_ptr<SpriteFont>> m_fonts;
 #pragma endregion
 
 #pragma region InternalGetters
 	static std::unordered_map<const wchar_t*, std::shared_ptr<Material>>& GetMaterials() { return m_materials; }
+
+	static std::unordered_map<const wchar_t*, std::shared_ptr<ObjFileModel>>& GetModels() { return m_models; }
+
+	static std::unordered_map<const wchar_t*, std::shared_ptr<SpriteFont>>& GetFonts() { return m_fonts; }
 #pragma endregion
 };
 
